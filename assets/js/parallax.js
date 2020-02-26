4b98e687e368c1e7fe83797d518135694eedfe64
iantra <iantrasolari@gmail.com>
2020-02-26 20:16:51 +0100

    Added parallax javascript and css

---
+++ assets/js/parallax.js
@@ -0,0 +1,10 @@
+$(window).ready(function () {
+    var div = $(".parallax");
+    var width = div.width();
+    div.css('height', width / 2);
+});
+$(window).scroll(function () {
+    var scrollval = $(this).scrollTop();    // It will return scroll value
+    $(".parallax").css("background-position", '50% ' + scrollval / 10 + '%');
+    console.log('50% ' + (50 + scrollval / 2) + '%;')
+});

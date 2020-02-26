be8f2ce24e5ce574e85d4f76188b6fa8ff4dbab1
iantra <iantrasolari@gmail.com>
2020-02-26 21:25:21 +0100

    javascript starting position now correct

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -3,8 +3,8 @@ $(window).ready(function () {
     var width = div.width();
     div.css('height', width / 2);
 });
+
 $(window).scroll(function () {
     var scrollval = $(this).scrollTop();    // It will return scroll value
-    $(".parallax").css("background-position", '50% ' + scrollval / 10 + '%');
-    console.log('50% ' + (50 + scrollval / 2) + '%;')
+    $(".parallax").css("background-position", '50% ' + (50 + scrollval / 20) + '%');
 });

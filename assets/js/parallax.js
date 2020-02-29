078c46f1cdbb4d9a69d2b873fcf098d3346f756a
iantra <iantrasolari@gmail.com>
2020-03-01 00:20:50 +0100

    Fixed resizing and scroll bar issue

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,8 +1,20 @@
-$(window).ready(function () {
+$(document).ready(update);
+$(window).resize(update);
+
+function update() {
     var div = $(".parallax");
     var width = div.width();
     div.css('height', width / 2);
-});
+
+    var body = $('body');
+    var normalwidth = 0;
+    var scrollwidth = 0;
+    if (body.prop('scrollHeight') > body.height()) {
+        normalwidth = window.innerWidth;
+        scrollwidth = normalwidth - body.width();
+        $('.container').css({ paddingLeft: scrollwidth + 'px' });
+    }
+}
 
 $(window).scroll(function () {
     var scrollval = $(this).scrollTop();    // It will return scroll value
